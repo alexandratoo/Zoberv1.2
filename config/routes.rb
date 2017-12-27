e10744a0d4c4a732de7be4a580e1b48ce07cdd95
Alexandra Tooley <sandratooley@rocketmail.com>
2017-12-27 15:21:00 -0700

    added static pages about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
 
 
+  get 'static/about'
+
+  get 'static/contact'
+
   get 'blog/index'
 
   get 'blog/each'
@@ -15,9 +19,11 @@ Rails.application.routes.draw do
   get 'individual' => 'houses#individual'
 
   get 'houses/home'
-
+get 'new' => 'users#new'
   root 'home_page#index'
   get 'signup' => 'providers#signup'
+  get 'about' => 'static#about'
+  get 'contact' => 'static#contact'
   resources :houses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
