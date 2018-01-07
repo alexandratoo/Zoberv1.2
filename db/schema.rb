c818663cca9b9c4e31a42f92aa54d72a80a13032
Andrew Eskenazi <drewesk@gmail.com>
2018-01-07 14:12:29 -0700

    refactor

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20171228010943) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "encrypted_password"
-    t.string "salt"
+    
+    t.string :email
+    t.string :username
+    t.string :password_digest
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
