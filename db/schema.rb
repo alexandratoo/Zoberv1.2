331db374eb3e3e5284f5d97551b6345a4380adf3
Andrew Eskenazi <drewesk@gmail.com>
2018-01-07 14:19:32 -0700

    git merge conflict fix
    using password_digest for bcrypt hash in users table

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20180107193534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"    
 
-=======
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "post"
