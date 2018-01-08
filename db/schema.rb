2dedbd2709c5aa8461a98825a0859cde4c6bbe3a
Alexandra Tooley <sandratooley@rocketmail.com>
2018-01-07 23:28:22 -0700

    added first name and last name to user db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108054852) do
+ActiveRecord::Schema.define(version: 20180107193534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,14 +81,13 @@ ActiveRecord::Schema.define(version: 20180108054852) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "encrypted_password"
-    t.string "salt"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "string"
   end
 
 end
