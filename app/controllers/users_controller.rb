fae12910a178032f8f2325488d965c538ce64a2a
Andrew Eskenazi <drewesk@gmail.com>
2018-01-08 13:58:56 -0700

    Login Logout Session
    * setting the current session in global state in application controller
    
    * refactor of syntax in users controller
    
    * super simple permissions check for both header partial and splash-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+        session[:id] = @user.id
       redirect_to root_url, notice: "Congrats, you've officially joined our cause"
     else
       render :new
@@ -15,6 +16,11 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :email,
+                                 :username,
+                                 :password,
+                                 :password_confirmation)
   end
 end
