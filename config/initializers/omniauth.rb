c6af294a7b9b94d94ac693db0f57c6cefc58330b
Andrew Eskenazi <drewesk@gmail.com>
2018-01-10 11:19:32 -0700

    bug fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '739899384767-ehrvsttlq8108b4gp4mvctev6kfokfeg.apps.googleusercontent.com', 'hZXkwY_Y364tnUsnHUQwXIgp', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '739899384767-ehrvsttlq8108b4gp4mvctev6kfokfeg.apps.googleusercontent.com', 'hZXkwY_Y364tnUsnHUQwXIgp', skip_jwt: true
 end # need to change key ownership to tyler, using my own google account for development
