ddd0ce7233c2c32cc996a78c130418adef6bc705
Alexandra Tooley <sandratooley@rocketmail.com>
2018-01-09 20:55:47 -0700

    ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,10 @@ class CommentsController < ApplicationController
     @comment = @blog.comments.create(params[:comment].permit(:name, :body))
     redirect_to blog_path(@blog)
   end
+  def destroy
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.find(params[:id])
+    @comment.destroy
+    redirect_to blog_path(@blog)
+  end
 end
