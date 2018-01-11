848503edb3f162745dd134385f4051be6e6f7653
Alexandra Tooley <sandratooley@rocketmail.com>
2018-01-11 13:20:51 -0700

    comments linked by user, displayed by username

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
   def create
     @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.create(params[:comment].permit(:name, :body))
-  #   if @comment.save
-  #     ;flash[:notice] = 'Comment was successfully created.'
-  #   redirect_to blog_path(@blog)
-  # else
-  #   flash[:notice] = "Error creating comment: #{@comment.errors}"
-  #
+@comment.user_id = current_user.id
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+    redirect_to blog_path(@blog)
+  else
+    flash[:notice] = "Error creating comment: #{@comment.errors}"
+end
   end
   def destroy
     @blog = Blog.find(params[:blog_id])
