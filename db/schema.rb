a704368001d18bd87dbe18ac8001f9e383b4d019
Alexandra Tooley <sandratooley@rocketmail.com>
2018-01-16 22:09:25 -0700

    making uui to make receipts more secure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127193536) do
+ActiveRecord::Schema.define(version: 20180127193537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20180127193536) do
     t.string "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
   create_table "users", force: :cascade do |t|
