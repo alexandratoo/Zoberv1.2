d7db6e8e06d6a12c6252cabf2980e9dd938af664
Alexandra Tooley <sandratooley@rocketmail.com>
2018-05-11 11:32:09 -0600

    database schema for blogs changed to accomodate selection, drop down added to new blog posting page

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180510200126) do
     t.string "image"
     t.string "name"
     t.string "website"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
