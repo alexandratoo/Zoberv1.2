7614e03abaf1ebd0d871d4f056b1a11f4c313d70
Alexandra Tooley <sandratooley@rocketmail.com>
2018-05-12 15:43:44 -0600

    table added to database with topics for blog, blog form changed to enable select for blog topic

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,31 +14,31 @@ yearly = Product.create(title: "yearly plan",
   sku: "yearly", download_url: "", description: %{<p>you now have the ability to advertise your home through zober. this is valid for one year. </p>
     });
 Topic.create!(topic: "spirituality");
-Topic.create!(topic: "fitness")
+Topic.create!(topic: "fitness");
 
-# image_array = ["https://images.craigslist.org/00n0n_cqc2jLGe1w0_600x450.jpg", "https://images.craigslist.org/00f0f_4lZ6YdfQnnu_600x450.jpg", "https://images.craigslist.org/00s0s_ceOPeAs9f25_600x450.jpg"]
-#
-#
-# index = 1
-# 43.times do
-#   image_array.each do |link|
-#     Image.create!({
-#     house_id: index,
-#     image: link
-#   })
-#   end
-# index += 1
-# end
-#
-# filter_id_array = [1, 4, 5, 8, 9, 10, 11, 12, 13, 15, 17, 18, 19, 20, 25, 29, 32, 38, 39, 41, 42, 47, 50, 55, 62, 65, 67, 68]
-#
-# index = 1
-# 43.times do
-#   filter_id_array.each do |filter|
-#     HouseFilter.create!({
-#     house_id: index,
-#     filter_id: filter
-#   })
-#   end
-# index += 1
-# end
+image_array = ["https://images.craigslist.org/00n0n_cqc2jLGe1w0_600x450.jpg", "https://images.craigslist.org/00f0f_4lZ6YdfQnnu_600x450.jpg", "https://images.craigslist.org/00s0s_ceOPeAs9f25_600x450.jpg"]
+
+
+index = 1
+43.times do
+  image_array.each do |link|
+    Image.create!({
+    house_id: index,
+    image: link
+  })
+  end
+index += 1
+end
+
+filter_id_array = [1, 4, 5, 8, 9, 10, 11, 12, 13, 15, 17, 18, 19, 20, 25, 29, 32, 38, 39, 41, 42, 47, 50, 55, 62, 65, 67, 68]
+
+index = 1
+43.times do
+  filter_id_array.each do |filter|
+    HouseFilter.create!({
+    house_id: index,
+    filter_id: filter
+  })
+  end
+index += 1
+end
